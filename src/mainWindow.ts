1cf573fb5e5118e1a24df21c9576734b3cce4145
HidekiHrk <hidekihiroki123@gmail.com>
2021-03-14 19:23:40 -0300

    Removed startup dev tools

--- src/mainWindow.ts
+++ src/mainWindow.ts
@@ -31,7 +31,7 @@ class MainWindow extends BrowserWindow {
         this.loadFile(FILE_PATH);
         this.webContents.userAgent =
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0";
-        this.webContents.openDevTools({ mode: "undocked" });
+        // this.webContents.openDevTools({ mode: "undocked" });
         this.on("ready-to-show", () => {
             this.show();
         });
