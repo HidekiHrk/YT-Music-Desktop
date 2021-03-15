2f911433e7819657e3f452243f8a3dd41b69b523
HidekiHrk <hidekihiroki123@gmail.com>
2021-03-15 19:00:55 -0300

    Building

--- install.sh
+++ install.sh
@@ -3,10 +3,12 @@ npm run build
 mkdir out/
 cp -r dist out/
 cp -r frontend out/
+cp -r node_modules out/
 cp package.json out/
 cp LICENSE out/
 cd out/
-npm install
 npm run package
-rm -r node_modules frontend package.json package-lock.json LICENSE dist
+rm -r node_modules frontend package.json LICENSE dist
+mv YouTubeMusicDesktop* YouTubeMusicDesktop
 cd ../
+cp res/* out/YouTubeMusicDesktop
